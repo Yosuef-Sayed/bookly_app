164c9c29cbd39dc772bd9e1f7848490747a9ce20
Yousef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-13 04:46:21 +0300

    Home custom app bar create

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, height: 18),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.search, size: 30),
+          ),
+        ],
+      ),
+    );
+  }
+}
