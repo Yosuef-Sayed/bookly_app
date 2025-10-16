14672b3dff43f7e02b3dc10b5678adc91526e904
Yosuef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-16 11:23:55 +0300

    Navigation refactor

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/features/home/presentation/views/screens/home_view.dart';
+import 'package:bookly/features/splash/presentation/views/screens/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = "/homeView";
+  static final router = GoRouter(
+    routes: [
+      GoRoute(path: "/", builder: (context, state) => const SplashView()),
+      GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+    ],
+  );
+}
