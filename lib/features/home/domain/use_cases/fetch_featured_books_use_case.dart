d3e774e5fb110649fa9691c061723317e1f68a03
Yosuef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-16 19:23:13 +0300

    UseCase class refactor

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repositories/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void params]) async {
+    //* Check Permission
+    return await homeRepo.fetchFeaturedBooks();
+  }
+}
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:bookly/features/search/presentation/views/widgets/search_result_list_view.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
