d4db56ecf9f8bbcebd40a7dafebc341f2e138e6f
Yosuef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-16 18:16:36 +0300

    Book entity create

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,17 @@
+class BookEntity {
+  final String bookImage;
+  final String bookTitle;
+  final String autherName;
+  final num price;
+  final num rating;
+  final num numberOfRates;
+
+  BookEntity(
+    this.bookImage,
+    this.bookTitle,
+    this.autherName,
+    this.price,
+    this.rating,
+    this.numberOfRates,
+  );
+}
