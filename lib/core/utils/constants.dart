43b37c5edec4007dc705631131ca58237ec7a3d5
Yosuef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-17 16:29:51 +0300

    BookEntityAdapter create and register

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/widgets.dart';
 const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(milliseconds: 300);
 const kGTSectraFine = "Sectra";
+const kFeaturedBox = "featured_box";
