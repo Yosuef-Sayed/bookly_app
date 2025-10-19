c28cc40f79d52e665e40fce57172ff6ab0de37d8
Yosuef-Sayed <yousef.sayed.hafeez@gmail.com>
2025-10-19 23:46:01 +0300

    Add MultiBlocProvider & DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/constants.dart';
+import 'package:bookly/core/utils/functions/setup_service_locator.dart';
 import 'package:bookly/core/utils/themes.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repositories/home_repo.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(BookEntityAdapter());
+
+  setupServiceLocator();
 
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
@@ -21,10 +30,24 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: AppThemes.theme,
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(locator.get<HomeRepo>()),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FetchNewestBooksUseCase(locator.get<HomeRepo>()),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: AppThemes.theme,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
